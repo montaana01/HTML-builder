7d3a39832e915da89591390fcd0bd1ff8ed0d6ed
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-18 17:54:08 +0300

    fix: solve some problems and update dirname

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,11 @@
 let fs = require('fs');
 const path = require('node:path');
 
-let pathToFile = path.join('./01-read-file/text.txt');
+let pathToFile = path.join(__dirname, 'text.txt');
 
 const readStream = fs.createReadStream(pathToFile);
 
 readStream.on('data', (data) => {
   console.log(data.toString());
+  readStream.close();
 });
-readStream.close();
