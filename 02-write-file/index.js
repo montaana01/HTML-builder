ab91dcd3bc253324fc8a1a98da8b2a9de66194e7
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-19 01:59:20 +0300

    feat: done with second task - write file with text

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const readline = require('node:readline');
+
+const userListener = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'write.txt'));
+
+console.log("Hello, User!\nWrite text below:\n");
+
+userListener.on('line', (line) => {
+  if (line === "exit" || line === "Exit" || line === "EXIT") {
+    userListener.close();
+  }
+  writeStream.write(`${line}\n`);
+})
+
+userListener.on("close", () =>{
+  console.log("Bye bye! See you later!");
+  writeStream.end();
+})
+
+process.on('SIGINT', () => {
+  userListener.close();
+});
